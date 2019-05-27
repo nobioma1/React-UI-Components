e8ccbbf5144de96fcab946ded9cdcb10840fab10
Noble Obioma <nobioma1@gmail.com>
2019-05-27 15:17:29 +0100

    STRETCH: Footer   -  add font awesome cdn link in index.html   - create footer component that return a div   - add icons with font awesome classnames respectively   - style footer - center main container div

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <i className="far fa-comment"></i>
+      <i className="fas fa-retweet"> 6 </i>
+      <i className="far fa-heart"> 4 </i>
+      <i className="far fa-envelope"></i>
+    </div>
+  );
+};
+
+export default Footer;
