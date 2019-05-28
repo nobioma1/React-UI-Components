24284b2f8fef68ae522b13c57ce97fcf523f3e8a
Noble Obioma <nobioma1@gmail.com>
2019-05-28 12:11:55 +0100

    Action Button Component:   - Create button component to take props value, that returns      a button element having a classname and  the value passed

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = ({ value }) => {
+  return <button className="action-btn">{value}</button>;
+};
+
+export default ActionButton;
