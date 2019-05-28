02b6e8d4e2618b3494aa268f17bdd757720666be
Noble Obioma <nobioma1@gmail.com>
2019-05-28 12:10:06 +0100

    Number Button Component:   - Create button component to take props value and style, that returns a      a button element having the classname and value passed

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = ({ value, styleName }) => {
+  return <button className={styleName}>{value}</button>;
+};
+
+export default NumberButton;
